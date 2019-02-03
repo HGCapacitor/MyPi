59590b423dac5b7ab1d259178a237890da567a5e
Herman Gerritsen <hgcapacitor@outlook.com>
2019-02-03 10:57:48 +0100

    cmake to crosscompile for rpi on ubuntu docker

--- Dockerfile.cmake
+++ Dockerfile.cmake
@@ -1,9 +1,10 @@
 FROM ubuntu:18.04 as builder
 RUN apt-get update
-RUN apt-get -y install build-essential cmake
+RUN apt-get -y install build-essential g++-8-arm-linux-gnueabihf cmake
 RUN mkdir -p /testprj
 WORKDIR /testprj
 COPY CMakeLists.txt /testprj/CMakeLists.txt
+COPY Toolchain-rpi3.cmake /testprj/Toolchain-rpi3.cmake
 COPY hello.cpp /testprj/hello.cpp
-RUN cmake .
+RUN cmake . -DCMAKE_TOOLCHAIN_FILE=Toolchain-rpi3.cmake
 RUN make
