ab0df410aab4826cbb14984842732f8c235f255c
Herman Gerritsen <hgcapacitor@outlook.com>
2019-02-07 19:53:04 +0100

    Working crosscompiled Blink example!

--- Dockerfile.cmake
+++ Dockerfile.cmake
@@ -1,10 +1,18 @@
 FROM ubuntu:18.04 as builder
 RUN apt-get update
-RUN apt-get -y install build-essential g++-8-arm-linux-gnueabihf cmake
+RUN apt-get -y install build-essential git vim g++-8-arm-linux-gnueabihf cmake
+RUN mkdir -p /wiringpi
+WORKDIR /wiringpi
+RUN git clone git://git.drogon.net/wiringPi
+WORKDIR /wiringpi/wiringPi/wiringPi
+COPY CMakeLists.wiringPi ./CMakeLists.txt
+COPY Toolchain-rpi3.cmake ./Toolchain-rpi3.cmake
+RUN cmake . -DCMAKE_TOOLCHAIN_FILE=Toolchain-rpi3.cmake
+RUN make
 RUN mkdir -p /testprj
 WORKDIR /testprj
-COPY CMakeLists.txt /testprj/CMakeLists.txt
-COPY Toolchain-rpi3.cmake /testprj/Toolchain-rpi3.cmake
+COPY CMakeLists.txt ./CMakeLists.txt
+COPY Toolchain-rpi3.cmake ./Toolchain-rpi3.cmake
 COPY hello.cpp /testprj/hello.cpp
 RUN cmake . -DCMAKE_TOOLCHAIN_FILE=Toolchain-rpi3.cmake
 RUN make
--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <wiringPi.h>
 
 int main (int argc, char *argv[])
 {
 	std::cout << "Hello world!!" << std::endl;
+	wiringPiSetup () ;
+	pinMode (0, OUTPUT) ;
+	for (;;)
+	{
+		digitalWrite (0, HIGH) ; delay (500) ;
+		digitalWrite (0,  LOW) ; delay (500) ;
+	}
+	return 0 ;
 }
